6ee78a905133bbc185ecc4bebebd1a62cd661a3c
Ahmed <112067109+AhmedMasOfficial@users.noreply.github.com>
2023-08-30 19:15:18 +0100

    .

--- script.js
+++ script.js
@@ -14,10 +14,39 @@ const card12 = document.getElementById("full-view-12");
 var next = document.getElementById("next-card");
 var prev = document.getElementById("prev-card");
 var t = document.getElementsByClassName("full");
+
 const grid = document.querySelector(".grid-container");
+const design_section = document.querySelector(".design");
+const video_section = document.querySelector(".video");
+const design3d_section = document.querySelector(".design3d");
+const web_section = document.querySelector(".web");
+const mobile_section = document.querySelector(".mobile");
+const desktop_section = document.querySelector(".desktop");
+const game_section = document.querySelector(".game");
+const ai_section = document.querySelector(".ai");
+
+const design_btn = document.querySelector(".design-btn");
+const video_btn = document.querySelector(".video-btn");
+const design3d_btn = document.querySelector(".design3d-btn");
+const web_btn = document.querySelector(".web-btn");
+const mobile_btn = document.querySelector(".mobile-btn");
+const desktop_btn = document.querySelector(".desktop-btn");
+const game_btn = document.querySelector(".game-btn");
+const ai_btn = document.querySelector(".ai-btn");
+
 const create_mini = document.querySelector("#createCard");
 const create_full = document.querySelector(".create-full");
 const create_form = document.getElementById("create-form");
+
+
+if (design_btn.classList.contains("empty")) design_btn.style.display = "none";
+if (video_btn.classList.contains("empty")) video_btn.style.display = "none";
+if (design3d_btn.classList.contains("empty")) design3d_btn.style.display = "none";
+if (web_btn.classList.contains("empty")) web_btn.style.display = "none";
+if (mobile_btn.classList.contains("empty")) mobile_btn.style.display = "none";
+if (desktop_btn.classList.contains("empty")) desktop_btn.style.display = "none";
+if (game_btn.classList.contains("empty")) game_btn.style.display = "none";
+if (ai_btn.classList.contains("empty")) ai_btn.style.display = "none";
 
 
 function blured(object) {
@@ -138,50 +167,105 @@ function open_creation() {
     background.style.display = "block";
 }
 
-function create_card() {
-    let image_url = createDirectLink(document.querySelector(".create-image").value);
-    // let image_url = document.querySelector(".create-image").value;
-    let title = document.getElementsByClassName("create-title");
-    let description = document.getElementsByClassName("create-description");
-    grid.removeChild(create_mini);
-    grid.innerHTML += `<div class="item" id="13" onclick="blured(this)">
-            <div class="image image13"> </div>
-            <div class="Card-Title : Welcome">Card-Title : Welcome</div>
-            <div class="tag-container">
-                <div class="tag tag-1">TAG 1</div>
-                <div class="tag tag-2">TAG 2</div>
-                <div class="tag tag-3">TAG 3</div>
-            </div>
-            <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi excepturi aut
-                quaerat magnam neque.
-                Voluptate.</div>
-            </div>`;
-    create_full.style.display = "none";
-    background.style.display = "none";
-    document.querySelector(".image13").style.backgroundImage = `url("${image_url}")`;
-    grid.appendChild(create_mini);
+// function create_card() {
+//     let image_url = createDirectLink(document.querySelector(".create-image").value);
+//     // let image_url = document.querySelector(".create-image").value;
+//     let title = document.getElementsByClassName("create-title");
+//     let description = document.getElementsByClassName("create-description");
+//     grid.removeChild(create_mini);
+//     grid.innerHTML += `<div class="item" id="13" onclick="blured(this)">
+//             <div class="image image13"> </div>
+//             <div class="Card-Title : Welcome">Card-Title : Welcome</div>
+//             <div class="tag-container">
+//                 <div class="tag tag-1">TAG 1</div>
+//                 <div class="tag tag-2">TAG 2</div>
+//                 <div class="tag tag-3">TAG 3</div>
+//             </div>
+//             <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi excepturi aut
+//                 quaerat magnam neque.
+//                 Voluptate.</div>
+//             </div>`;
+//     create_full.style.display = "none";
+//     background.style.display = "none";
+//     document.querySelector(".image13").style.backgroundImage = `url("${image_url}")`;
+//     grid.appendChild(create_mini);
+// }
+
+
+// function paste() {
+//     const paste = document.querySelector('.create-image');
+//     navigator.clipboard.readText().then((clipText) => (paste.value = clipText));
+// }
+
+
+
+function hideAll() {
+    design_section.style.display = "none";
+    video_section.style.display = "none";
+    design3d_section.style.display = "none";
+    web_section.style.display = "none";
+    mobile_section.style.display = "none";
+    desktop_section.style.display = "none";
+    game_section.style.display = "none";
+    ai_section.style.display = "none";
+
+    design_btn.classList.remove("active");
+    design3d_btn.classList.remove("active");
+    video_btn.classList.remove("active");
+    web_btn.classList.remove("active");
+    mobile_btn.classList.remove("active");
+    desktop_btn.classList.remove("active");
+    game_btn.classList.remove("active");
+    ai_btn.classList.remove("active");
 }
 
+function showDesign() {
+    hideAll();
+    design_section.style.display = "block";
+    design_btn.classList.add("active");
 
-function paste() {
-    const paste = document.querySelector('.create-image');
-    navigator.clipboard.readText().then((clipText) => (paste.value = clipText));
 }
 
+function showVideo() {
+    hideAll();
+    video_section.style.display = "block";
+    video_btn.classList.add("active");
 
-//============== skill list load ==================//
+}
 
-const skills = document.querySelectorAll("li-side");
-let current_skill = 0;
+function show3d() {
+    hideAll();
+    design3d_section.style.display = "block";
+    design3d_btn.classList.add("active");
 
-// skills.addEventListener("click", function(this){
-//     this.classList.add("active");
-// });
+}
+function showWeb() {
+    hideAll();
+    web_section.style.display = "block";
+    web_btn.classList.add("active");
 
+}
+function showMobile() {
+    hideAll();
+    mobile_section.style.display = "block";
+    mobile_btn.classList.add("active");
 
-function setActive(obj){
-    skills.forEach(element => {
-        element.classList.remove("active");
-    });
-    obj.classList.add("active");
+}
+function showDesktop() {
+    hideAll();
+    desktop_section.style.display = "block";
+    desktop_btn.classList.add("active");
+
+}
+function showGame() {
+    hideAll();
+    game_section.style.display = "block";
+    game_btn.classList.add("active");
+
+}
+function showAi() {
+    hideAll();
+    ai_section.style.display = "block";
+    ai_btn.classList.add("active");
+
 }
