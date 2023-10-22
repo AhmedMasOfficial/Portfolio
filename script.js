1430a1e2dc0e7327ac1baa749fe030dcb3a9d0d2
Ahmed <112067109+AhmedMasOfficial@users.noreply.github.com>
2023-10-22 20:11:02 +0100

    some fixes and updates

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const background = document.getElementById("blur-background");
-const card1 = document.getElementById("full-view-1");
-const card2 = document.getElementById("full-view-2");
-const card3 = document.getElementById("full-view-3");
-const card4 = document.getElementById("full-view-4");
-const card5 = document.getElementById("full-view-5");
-const card6 = document.getElementById("full-view-6");
-const card7 = document.getElementById("full-view-7");
-const card8 = document.getElementById("full-view-8");
-const card9 = document.getElementById("full-view-9");
-const card10 = document.getElementById("full-view-10");
-const card11 = document.getElementById("full-view-11");
-const card12 = document.getElementById("full-view-12");
 var next = document.getElementById("next-card");
 var prev = document.getElementById("prev-card");
-var t = document.getElementsByClassName("full");
 
 const grid = document.querySelector(".grid-container");
 const design_section = document.querySelector(".design");
@@ -35,115 +22,158 @@ const game_btn = document.querySelector(".game-btn");
 const ai_btn = document.querySelector(".ai-btn");
 
 const create_mini = document.querySelector("#createCard");
-const create_full = document.querySelector(".create-full");
-const create_form = document.getElementById("create-form");
+// const create_full = document.querySelector(".create-full");
+// const create_form = document.getElementById("create-form");
 
 
-if (design_btn.classList.contains("empty")) design_btn.style.display = "none";
-if (video_btn.classList.contains("empty")) video_btn.style.display = "none";
-if (design3d_btn.classList.contains("empty")) design3d_btn.style.display = "none";
-if (web_btn.classList.contains("empty")) web_btn.style.display = "none";
-if (mobile_btn.classList.contains("empty")) mobile_btn.style.display = "none";
-if (desktop_btn.classList.contains("empty")) desktop_btn.style.display = "none";
-if (game_btn.classList.contains("empty")) game_btn.style.display = "none";
-if (ai_btn.classList.contains("empty")) ai_btn.style.display = "none";
+// if (design_btn.classList.contains("empty")) design_btn.style.display = "none";
+// if (video_btn.classList.contains("empty")) video_btn.style.display = "none";
+// if (design3d_btn.classList.contains("empty")) design3d_btn.style.display = "none";
+// if (web_btn.classList.contains("empty")) web_btn.style.display = "none";
+// if (mobile_btn.classList.contains("empty")) mobile_btn.style.display = "none";
+// if (desktop_btn.classList.contains("empty")) desktop_btn.style.display = "none";
+// if (game_btn.classList.contains("empty")) game_btn.style.display = "none";
+// if (ai_btn.classList.contains("empty")) ai_btn.style.display = "none";
+
+var full = document.getElementsByClassName("full")[0];
+let items = document.getElementsByClassName("item-design");
 
 
-function blured(object) {
+////////////////////////////////////////////////////////////////
+//The functions that when you click on a category, the old one will be removed and the new one will be displayed
+////////////////////////////////////////////////////////////////
+
+
+function showDesign() {
+    hideAll();
+    items = document.getElementsByClassName("item-design");
+    design_section.style.display = "block";
+    design_btn.classList.add("active");
+}
+function showVideo() {
+    hideAll();
+    items = document.getElementsByClassName("item-video");
+    video_section.style.display = "block";
+    video_btn.classList.add("active");
+}
+function show3d() {
+    hideAll();
+    items = document.getElementsByClassName("item-3d");
+    design3d_section.style.display = "block";
+    design3d_btn.classList.add("active");
+
+}
+function showWeb() {
+    hideAll();
+    items = document.getElementsByClassName("item-web")
+    web_section.style.display = "block";
+    web_btn.classList.add("active");
+
+}
+function showMobile() {
+    hideAll();
+    items = document.getElementsByClassName("item-mobile");
+    mobile_section.style.display = "block";
+    mobile_btn.classList.add("active");
+
+}
+function showDesktop() {
+    hideAll();
+    items = document.getElementsByClassName("item-desktop");
+    desktop_section.style.display = "block";
+    desktop_btn.classList.add("active");
+
+}
+function showGame() {
+    hideAll();
+    items = document.getElementsByClassName("item-game");
+    game_section.style.display = "block";
+    game_btn.classList.add("active");
+
+}
+function showAi() {
+    hideAll();
+    items = document.getElementsByClassName("item-ai");
+    ai_section.style.display = "block";
+    ai_btn.classList.add("active");
+
+}
+
+function hideAll() {
+    design_section.style.display = "none";
+    video_section.style.display = "none";
+    design3d_section.style.display = "none";
+    web_section.style.display = "none";
+    mobile_section.style.display = "none";
+    desktop_section.style.display = "none";
+    game_section.style.display = "none";
+    ai_section.style.display = "none";
+
+    design_btn.classList.remove("active");
+    design3d_btn.classList.remove("active");
+    video_btn.classList.remove("active");
+    web_btn.classList.remove("active");
+    mobile_btn.classList.remove("active");
+    desktop_btn.classList.remove("active");
+    game_btn.classList.remove("active");
+    ai_btn.classList.remove("active");
+}
+
+////////////////////////////////////////////////////////////////
+//the functions that when you click on the card, the card will be displayed in full screen
+////////////////////////////////////////////////////////////////
+
+function blured(object){
+    var source = object.src;
+    full.style.backgroundImage = `url("${source}")`;
+    full.style.display = "block";
     background.style.display = "block";
     next.style.display = "block";
     prev.style.display = "block";
-    if (object == document.getElementById("1")) {
-        card1.style.display = "block";
+
+    for (let i = 0; i < items.length; i++) {
+        if (source == items[i].src) {
+            current = i;
+            break;
+        }
+    }
+
+    if (current == 0) {
         prev.style.display = "none";
-        current = 1;
     }
-    if (object == document.getElementById("2")) {
-        card2.style.display = "block";
-        current = 2;
+    else {
+        prev.style.display = "block";
     }
-    if (object == document.getElementById("3")) {
-        card3.style.display = "block";
-        current = 3;
-    }
-    if (object == document.getElementById("4")) {
-        card4.style.display = "block";
-        current = 4;
-    }
-    if (object == document.getElementById("5")) {
-        card5.style.display = "block";
-        current = 5;
-    }
-    if (object == document.getElementById("6")) {
-        card6.style.display = "block";
-        current = 6;
-    }
-    if (object == document.getElementById("7")) {
-        card7.style.display = "block";
-        current = 7;
-    }
-    if (object == document.getElementById("8")) {
-        card8.style.display = "block";
-        current = 8;
-    }
-    if (object == document.getElementById("9")) {
-        card9.style.display = "block";
-        current = 9;
-    }
-    if (object == document.getElementById("10")) {
-        card10.style.display = "block";
-        current = 10;
-    }
-    if (object == document.getElementById("11")) {
-        card11.style.display = "block";
-        current = 11;
-    }
-    if (object == document.getElementById("12")) {
-        card12.style.display = "block";
+
+    if (current + 1 == items.length) {
         next.style.display = "none";
-        current = 12;
+    }
+    else {
+        next.style.display = "block";
     }
 
-
-}
-function hide() {
-    background.style.display = "none";
-    next.style.display = "none";
-    prev.style.display = "none";
-    card1.style.display = "none";
-    card2.style.display = "none";
-    card3.style.display = "none";
-    card4.style.display = "none";
-    card5.style.display = "none";
-    card6.style.display = "none";
-    card7.style.display = "none";
-    card8.style.display = "none";
-    card9.style.display = "none";
-    card10.style.display = "none";
-    card11.style.display = "none";
-    card12.style.display = "none";
-    create_full.style.display = "none";
-    create_form.reset();
 }
 
 function to_next() {
-    t[current - 1].style.display = "none";
-    t[current].style.display = "block";
     prev.style.display = "block";
+    source = items[current + 1].src;
+    full.style.backgroundImage = `url("${source}")`;
     current++;
-    if (current == 12) {
+
+    if (current + 1 == items.length) {
         next.style.display = "none";
     }
     else {
         next.style.display = "block";
     }
 }
+
 function to_prev() {
-    t[current - 1].style.display = "none";
-    t[current - 2].style.display = "block";
     next.style.display = "block";
+    source = items[current - 1].src;
+    full.style.backgroundImage = `url("${source}")`;
     current--;
-    if (current == 1) {
+    if (current == 0) {
         prev.style.display = "none";
     }
     else {
@@ -152,20 +182,37 @@ function to_prev() {
 }
 
 
-function createDirectLink(ch) {
-    /*  normal :
-        https://drive.google.com/file/d/1qvVlqyl3qi6vXSUGVnYVD2nsyYM-4vC0/view?usp=drive_link
-        direct :
-        https://drive.google.com/uc?export=download&id=1qvVlqyl3qi6vXSUGVnYVD2nsyYM-4vC0
-    */
-    const myArray = ch.split("/");
-    return "https://drive.google.com/uc?export=download&id=" + myArray[myArray.lenght - 2];
+function hide() {
+    background.style.display = "none";
+    next.style.display = "none";
+    prev.style.display = "none";
+    full.style.display = "none";
+    // create_full.style.display = "none";
+    // create_form.reset();
 }
 
-function open_creation() {
-    create_full.style.display = "block";
-    background.style.display = "block";
-}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//the functions that when you click on the create button, the create form will be displayed 
+// and you will input a new card.
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function createDirectLink(ch) {
+//     /*  normal :
+//         https://drive.google.com/file/d/1qvVlqyl3qi6vXSUGVnYVD2nsyYM-4vC0/view?usp=drive_link
+//         direct :
+//         https://drive.google.com/uc?export=download&id=1qvVlqyl3qi6vXSUGVnYVD2nsyYM-4vC0
+//     */
+//     const myArray = ch.split("/");
+//     return "https://drive.google.com/uc?export=download&id=" + myArray[myArray.lenght - 2];
+// }
+
+// function open_creation() {
+//     create_full.style.display = "block";
+//     background.style.display = "block";
+// }
 
 // function create_card() {
 //     let image_url = createDirectLink(document.querySelector(".create-image").value);
@@ -173,7 +220,7 @@ function open_creation() {
 //     let title = document.getElementsByClassName("create-title");
 //     let description = document.getElementsByClassName("create-description");
 //     grid.removeChild(create_mini);
-//     grid.innerHTML += `<div class="item" id="13" onclick="blured(this)">
+//     grid.innerHTML += `<div class="item_design" id="13" onclick="blured(this)">
 //             <div class="image image13"> </div>
 //             <div class="Card-Title : Welcome">Card-Title : Welcome</div>
 //             <div class="tag-container">
@@ -199,73 +246,5 @@ function open_creation() {
 
 
 
-function hideAll() {
-    design_section.style.display = "none";
-    video_section.style.display = "none";
-    design3d_section.style.display = "none";
-    web_section.style.display = "none";
-    mobile_section.style.display = "none";
-    desktop_section.style.display = "none";
-    game_section.style.display = "none";
-    ai_section.style.display = "none";
 
-    design_btn.classList.remove("active");
-    design3d_btn.classList.remove("active");
-    video_btn.classList.remove("active");
-    web_btn.classList.remove("active");
-    mobile_btn.classList.remove("active");
-    desktop_btn.classList.remove("active");
-    game_btn.classList.remove("active");
-    ai_btn.classList.remove("active");
-}
 
-function showDesign() {
-    hideAll();
-    design_section.style.display = "block";
-    design_btn.classList.add("active");
-
-}
-
-function showVideo() {
-    hideAll();
-    video_section.style.display = "block";
-    video_btn.classList.add("active");
-
-}
-
-function show3d() {
-    hideAll();
-    design3d_section.style.display = "block";
-    design3d_btn.classList.add("active");
-
-}
-function showWeb() {
-    hideAll();
-    web_section.style.display = "block";
-    web_btn.classList.add("active");
-
-}
-function showMobile() {
-    hideAll();
-    mobile_section.style.display = "block";
-    mobile_btn.classList.add("active");
-
-}
-function showDesktop() {
-    hideAll();
-    desktop_section.style.display = "block";
-    desktop_btn.classList.add("active");
-
-}
-function showGame() {
-    hideAll();
-    game_section.style.display = "block";
-    game_btn.classList.add("active");
-
-}
-function showAi() {
-    hideAll();
-    ai_section.style.display = "block";
-    ai_btn.classList.add("active");
-
-}
