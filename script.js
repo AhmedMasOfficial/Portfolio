3f9c431bd24d6aecee45b21705a5d2a361627620
Ahmed <112067109+AhmedMasOfficial@users.noreply.github.com>
2023-10-25 22:39:22 +0100

    .

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const background = document.getElementById("blur-background");
 var next = document.getElementById("next-card");
 var prev = document.getElementById("prev-card");
 
-const grid_video = document.querySelector(".grid-container-video");
+
 const design_section = document.querySelector(".design");
 const video_section = document.querySelector(".video");
 const design3d_section = document.querySelector(".design3d");
@@ -21,10 +21,9 @@ const desktop_btn = document.querySelector(".desktop-btn");
 const game_btn = document.querySelector(".game-btn");
 const ai_btn = document.querySelector(".ai-btn");
 
-const create_mini = document.querySelector("#createCard");
+// const create_mini = document.querySelector("#createCard");
 // const create_full = document.querySelector(".create-full");
 // const create_form = document.getElementById("create-form");
-
 
 // if (design_btn.classList.contains("empty")) design_btn.style.display = "none";
 // if (video_btn.classList.contains("empty")) video_btn.style.display = "none";
@@ -120,69 +119,6 @@ function hideAll() {
     ai_btn.classList.remove("active");
 }
 
-////////////////////////////////////////////////////////////////
-//the functions that when you click on the card, the card will be displayed in full screen
-////////////////////////////////////////////////////////////////
-
-function blured(object){
-    var source = object.src;
-    full.style.backgroundImage = `url("${source}")`;
-    full.style.display = "block";
-    background.style.display = "block";
-    next.style.display = "block";
-    prev.style.display = "block";
-
-    for (let i = 0; i < items.length; i++) {
-        if (source == items[i].src) {
-            current = i;
-            break;
-        }
-    }
-
-    if (current == 0) {
-        prev.style.display = "none";
-    }
-    else {
-        prev.style.display = "block";
-    }
-
-    if (current + 1 == items.length) {
-        next.style.display = "none";
-    }
-    else {
-        next.style.display = "block";
-    }
-
-}
-
-function to_next() {
-    prev.style.display = "block";
-    source = items[current + 1].src;
-    full.style.backgroundImage = `url("${source}")`;
-    current++;
-
-    if (current + 1 == items.length) {
-        next.style.display = "none";
-    }
-    else {
-        next.style.display = "block";
-    }
-}
-
-function to_prev() {
-    next.style.display = "block";
-    source = items[current - 1].src;
-    full.style.backgroundImage = `url("${source}")`;
-    current--;
-    if (current == 0) {
-        prev.style.display = "none";
-    }
-    else {
-        prev.style.display = "block";
-    }
-}
-
-
 function hide() {
     background.style.display = "none";
     next.style.display = "none";
@@ -191,6 +127,10 @@ function hide() {
     // create_full.style.display = "none";
     // create_form.reset();
 }
+
+
+
+
 
 
 
@@ -246,18 +186,103 @@ function hide() {
 // }
 
 
+const grid_design = document.querySelector(".grid-container-design");
+let item_image = ["images/IG-1.png","images/IG-2.png","images/IG-3.png", "images/IG-4-3.png","images/pic1-2.png", "images/pic2-1.png","images/insta.png", "images/posts.ai_1200_630_px_1500_1500_px_1.png",
+    "images/340653563_949680539392767_4660127515563206705_n.webp",
+    "images/2-11.png", "images/113.png", "images/1122.png",
+    "images/2-logo nmtc+.png", "images/+Artboard 32_2.png", "images/official logo.png",
+    "images/insta_ipsas_cyber.png", "images/insta3.png", "images/ezfhbze.jpg",
+    "images/profil-pic.png", "images/1.jpg", "images/2.jpg", "images/3.jpg",
+    "images/4.jpg", "images/5.jpg", "images/6.jpg", "images/7.jpg", "images/8.jpg",
+    "images/9.jpg", "images/10.jpg", "images/11.jpg", "images/12.jpg"];
 
-//https://drive.google.com/file/d/1GbzRTXswwPC4PtnFO50V2boMbpjk4WHR/preview
+for (var i = 0; i < item_image.length; i++) {
+    grid_design.innerHTML += `<img class="item item-design" onclick="blured(this)" src="${item_image[i]}">`;
+}
 
+// item_image.forEach(element => {
+//     grid_design.innerHTML += `<img class="item item-design" onclick="blured(this)" src="${element}">`;
+// })
+
+
+////////////////////////////////////////////////////////////////
+//the functions that when you click on the card, the card will be displayed in full screen
+////////////////////////////////////////////////////////////////
+
+function blured(object) {
+    var source = object.src;
+    full.innerHTML = "";
+    full.style.backgroundImage = `url("${source}")`;
+    full.style.display = "block";
+    background.style.display = "block";
+    next.style.display = "block";
+    prev.style.display = "block";
+
+    for (let i = 0; i < items.length; i++) {
+        if (source == items[i].src) {
+            current = i;
+            break;
+        }
+    }
+
+    if (current == 0) {
+        prev.style.display = "none";
+    }
+    else {
+        prev.style.display = "block";
+    }
+
+    if (current + 1 == items.length) {
+        next.style.display = "none";
+    }
+    else {
+        next.style.display = "block";
+    }
+
+}
+
+
+function to_next() {
+    prev.style.display = "block";
+    source = items[current + 1].src;
+    full.style.backgroundImage = `url("${source}")`;
+    current++;
+
+    if (current + 1 == items.length) {
+        next.style.display = "none";
+    }
+    else {
+        next.style.display = "block";
+    }
+}
+
+function to_prev() {
+    next.style.display = "block";
+    source = items[current - 1].src;
+    full.style.backgroundImage = `url("${source}")`;
+    current--;
+    if (current == 0) {
+        prev.style.display = "none";
+    }
+    else {
+        prev.style.display = "block";
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 //create Video Items and tables
 ////////////////////////////////////////////////////////////////////////
 
+const grid_video = document.querySelector(".grid-container-video");
+
 let video_links = ["https://drive.google.com/file/d/1GbzRTXswwPC4PtnFO50V2boMbpjk4WHR/preview",
-    "https://drive.google.com/uc?export=download&id=1oCNEdWflL9xfnK-8C6MllR46-fj3KaVI",
-    "https://drive.google.com/uc?export=download&id=1mrMxbGyRKBSGDQ7Wb7SFF3pbH4yJlDAW",
-    "https://drive.google.com/uc?export=download&id=1wXHQGfHHFYQo7qeqEmB3TvKGjsQq8vP6",
-    "https://drive.google.com/uc?export=download&id=15LB2BAsFTiqxuI07hT2k4owODvKqyC6d"
+    "https://drive.google.com/file/d/1oCNEdWflL9xfnK-8C6MllR46-fj3KaVI/preview",
+    "https://drive.google.com/file/d/1mrMxbGyRKBSGDQ7Wb7SFF3pbH4yJlDAW/preview",
+    "https://drive.google.com/file/d/1wXHQGfHHFYQo7qeqEmB3TvKGjsQq8vP6/preview",
+    "https://drive.google.com/file/d/15LB2BAsFTiqxuI07hT2k4owODvKqyC6d/preview"
 ];
 
 let video_thubnails = ["Video/Screenshot 2023-08-30 185101.png",
@@ -266,7 +291,7 @@ let video_thubnails = ["Video/Screenshot 2023-08-30 185101.png",
     "Video/Screenshot 2023-08-30 184828.png",
     "Video/Screenshot 2023-08-30 184920.png"
 ];
-
+//this loop creates the grid items of the video categorie
 for (let i = 0; i < video_thubnails.length; i++) {
     grid_video.innerHTML += `<div class="item-container">
     <img class="item" onclick="blured_video(${i})" src="${video_thubnails[i]}">
@@ -274,7 +299,15 @@ for (let i = 0; i < video_thubnails.length; i++) {
 </div>`;
 }
 
-function blured_video(i){
+
+////////////////////////////////////////////////////////////////
+// Video Fullscreen view
+////////////////////////////////////////////////////////////////
+
+
+let current = 0;
+function blured_video(i) {
+    full.style.backgroundImage = "none";
     current = i;
     full.innerHTML = `<iframe src="${video_links[current]}" style="aspect-ratio:16/9;width:60vw;transform:translate(0,10%)" allow="autoplay" allowfullscreen></iframe>`;
     full.style.display = "block";
@@ -282,53 +315,64 @@ function blured_video(i){
     next.style.display = "block";
     prev.style.display = "block";
 
-    for (let i = 0; i < vid_array.length; i++) {
-        if (source == items[i].src) {
-            current = i;
-            break;
-        }
+    if (current == 0) {
+        prev.style.display = "none";
+    }
+    else {
+        prev.style.display = "block";
     }
 
-    // if (current == 0) {
-    //     prev.style.display = "none";
-    // }
-    // else {
-    //     prev.style.display = "block";
-    // }
-
-    // if (current + 1 == items.length) {
-    //     next.style.display = "none";
-    // }
-    // else {
-    //     next.style.display = "block";
-    // }
+    if (current + 1 == video_links.length) {
+        next.style.display = "none";
+    }
+    else {
+        next.style.display = "block";
+    }
 
 }
 
-// function to_next() {
-//     prev.style.display = "block";
-//     source = items[current + 1].src;
-//     full.style.backgroundImage = `url("${source}")`;
-//     current++;
+next.addEventListener("click", () => {
+    if (video_btn.classList.contains("active")) {
+        to_next_vid(current);
+    }
+    else {
+        to_next();
+    }
+});
 
-//     if (current + 1 == items.length) {
-//         next.style.display = "none";
-//     }
-//     else {
-//         next.style.display = "block";
-//     }
-// }
+prev.addEventListener("click", () => {
+    if (video_btn.classList.contains("active")) {
+        to_prev_vid(current);
+    }
+    else {
+        to_prev();
+    }
+});
 
-// function to_prev() {
-//     next.style.display = "block";
-//     source = items[current - 1].src;
-//     full.style.backgroundImage = `url("${source}")`;
-//     current--;
-//     if (current == 0) {
-//         prev.style.display = "none";
-//     }
-//     else {
-//         prev.style.display = "block";
-//     }
-// }
+
+function to_next_vid(i) {
+    current = i;
+    prev.style.display = "block";
+    full.innerHTML = `<iframe src="${video_links[++current]}" style="aspect-ratio:16/9;width:60vw;transform:translate(0,10%)" allow="autoplay" allowfullscreen></iframe>`;
+
+    if (current + 1 == video_links.length) {
+        next.style.display = "none";
+    }
+    else {
+        next.style.display = "block";
+    }
+}
+
+function to_prev_vid(i) {
+    current = i;
+    next.style.display = "block";
+    full.innerHTML = `<iframe src="${video_links[--current]}" style="aspect-ratio:16/9;width:60vw;transform:translate(0,10%)" allow="autoplay" allowfullscreen></iframe>`;
+
+    if (current == 0) {
+        prev.style.display = "none";
+    }
+    else {
+        prev.style.display = "block";
+    }
+}
 
