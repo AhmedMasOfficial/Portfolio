b35f30e994b7f10d41bed0bc40969932c6b226b3
Ahmed <112067109+AhmedMasOfficial@users.noreply.github.com>
2024-02-11 19:19:06 +0100

    .

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -2,19 +2,33 @@
 //----------------------------------------------------------------
 //animation when shown
 
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll(".hidden-move");
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            console.log(entry.target);
             entry.target.classList.add("show");
             // observer.unobserve(entry.target);
         }
-        else{
-            entry.target.classList.remove("show");
-        }
+        // else{
+        //     entry.target.classList.remove("show");
+        // }
     });
-});
+}, { threshold: 0.5 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+/* another intersection observer*/
+
+const hiddenElements2 = document.querySelectorAll(".hidden");
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show2");
+        }
+    });
+}, { threshold: 0.5 });
+
+hiddenElements2.forEach((el) => observer2.observe(el));
